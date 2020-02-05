9502eea07650c51f571171fe807c8f936f22d3b1
jaxchan25 <446769105@qq.com>
2020-02-05 13:26:56 +0800

    添加注册登录功能的api文档解释

--- README.md
+++ README.md
@@ -63,21 +63,21 @@ go run main.go
 
 ### 标准语法
 ```
-    /**
-    * showdoc
-    * @catalog 测试文档/用户相关
-    * @title 用户登录
-    * @description 用户登录的接口
-    * @method get
-    * @url https://www.showdoc.cc/home/user/login
-    * @header token 可选 string 设备token 
-    * @param username 必选 string 用户名 
-    * @param password 必选 string 密码  
-    * @param name 可选 string 用户昵称  
-    * @return {"error_code":0,"data":{"uid":"1","username":"12154545","name":"吴系挂","groupid":2,"reg_time":"1436864169","last_login_time":"0"}}
-    * @return_param groupid int 用户组id
-    * @return_param name string 用户昵称
-    * @remark 这里是备注信息
-    * @number 99
-    */
+        /**
+        * showdoc
+        * @catalog 测试文档/用户相关
+        * @title 测试api文档自动生成
+        * @description 测试api文档自动生成
+        * @method get
+        * @url https://www.showdoc.cc/home/user/login
+        * @header token 可选 string 设备token 
+        * @param username 必选 string 用户名 
+        * @param password 必选 string 密码  
+        * @param name 可选 string 用户昵称  
+        * @return {"error_code":0,"data":{"uid":"1","username":"12154545","name":"吴系挂","groupid":2,"reg_time":"1436864169","last_login_time":"0"}}
+        * @return_param groupid int 用户组id
+        * @return_param name string 用户昵称
+        * @remark 这里是备注信息
+        * @number 99
+        */
 ```
--- server/router.go
+++ server/router.go
@@ -1,9 +1,9 @@
 package server
 
 import (
-	"os"
 	"borderland/api"
 	"borderland/middleware"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,20 +20,69 @@ func NewRouter() *gin.Engine {
 	// 路由
 	v1 := r.Group("/api/v1")
 	{
+		/**
+		* showdoc
+		* @catalog 测试文档/运行测试
+		* @title 服务器运行
+		* @description 测试服务器是否运行
+		* @method post
+		* @url /api/v1/ping
+		* @return {"code":0,"msg":"Pong"}
+		 */
 		v1.POST("ping", api.Ping)
 
-		// 用户登录
+		/**
+		* showdoc
+		* @catalog 用户相关
+		* @title 用户注册
+		* @description 用户注册api
+		* @method post
+		* @url /api/v1/user/register
+		* @param nickname 必选 string 昵称
+		* @param user_name 必选 string 用户名
+		* @param password 必选 string 密码
+		* @param password_confirm 必选 string 再次确认密码
+		* @return {"code":0,"data":{"id":1,"user_name":"explorer","nickname":"lovelyjax","status":"active","avatar":"","created_at":1580879384},"msg":""}
+		 */
 		v1.POST("user/register", api.UserRegister)
 
-		// 用户登录
+		/**
+		* showdoc
+		* @catalog 用户相关
+		* @title 用户登录
+		* @description 用户登录api
+		* @method post
+		* @url /api/v1/user/login
+		* @param user_name 必选 string 用户名
+		* @param password 必选 string 密码
+		* @return  {"code":0,"data":{"id":1,"user_name":"explorer","nickname":"lovelyjax","status":"active","avatar":"","created_at":1580879384},"msg":""}
+		 */
 		v1.POST("user/login", api.UserLogin)
 
 		// 需要登录保护的
 		auth := v1.Group("")
 		auth.Use(middleware.AuthRequired())
 		{
-			// User Routing
+			/**
+			* showdoc
+			* @catalog 用户相关
+			* @title 获取当前用户信息
+			* @description 获取当前用户信息api
+			* @method get
+			* @url /api/v1/user/me
+			* @return  {"code":0,"data":{"id":1,"user_name":"explorer","nickname":"lovelyjax","status":"active","avatar":"","created_at":1580879384},"msg":""}
+			 */
 			auth.GET("user/me", api.UserMe)
+
+			/**
+			* showdoc
+			* @catalog 用户相关
+			* @title 注销登录
+			* @description 注销登录api
+			* @method delete
+			* @url /api/v1/user/logout
+			* @return {"code":0,"msg":"登出成功"}
+			 */
 			auth.DELETE("user/logout", api.UserLogout)
 		}
 	}
