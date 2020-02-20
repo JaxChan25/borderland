cc8ff0ae2b8af1b37c6de72883771dc992eb0bcc
jaxchan25 <446769105@qq.com>
2020-02-20 13:16:11 +0800

    finish catalog api

--- service/list_article_service.go
+++ service/list_article_service.go
@@ -7,8 +7,9 @@ import (
 
 // ListArticleService 文章列表的服务
 type ListArticleService struct {
-	Limit int `form:"limit"`
-	Start int `form:"start"`
+	Limit   int    `form:"limit"`
+	Start   int    `form:"start"`
+	Catalog string `form:"catalog"`
 }
 
 // List 展示文章列表
@@ -21,21 +22,42 @@ func (service *ListArticleService) List() serializer.Response {
 		service.Limit = 6
 	}
 
-	//文章总数
-	if err := model.DB.Model(model.Article{}).Count(&total).Error; err != nil {
-		return serializer.Response{
-			Code:  50000,
-			Msg:   "数据库查询错误",
-			Error: err.Error(),
+	if service.Catalog == "" {
+		//文章总数
+		if err := model.DB.Model(model.Article{}).Count(&total).Error; err != nil {
+			return serializer.Response{
+				Code:  50000,
+				Msg:   "数据库查询错误",
+				Error: err.Error(),
+			}
 		}
-	}
 
-	if err := model.DB.Limit(service.Limit).Offset(service.Start).Find(&articles).Error; err != nil {
-		return serializer.Response{
-			Code:  50000,
-			Msg:   "数据库查询错误",
-			Error: err.Error(),
+		if err := model.DB.Limit(service.Limit).Offset(service.Start).Find(&articles).Error; err != nil {
+			return serializer.Response{
+				Code:  50000,
+				Msg:   "数据库查询错误",
+				Error: err.Error(),
+			}
 		}
+
+	} else { //catalog不为空
+
+		if err := model.DB.Model(model.Article{}).Where("catalog = ?", service.Catalog).Count(&total).Error; err != nil {
+			return serializer.Response{
+				Code:  50000,
+				Msg:   "数据库查询错误",
+				Error: err.Error(),
+			}
+		}
+
+		if err := model.DB.Limit(service.Limit).Offset(service.Start).Where("catalog = ?", service.Catalog).Find(&articles).Error; err != nil {
+			return serializer.Response{
+				Code:  50000,
+				Msg:   "数据库查询错误",
+				Error: err.Error(),
+			}
+		}
+
 	}
 
 	return serializer.BuildListResponse(serializer.BuildArticlesResponse(articles), uint(total))
